1b639a3377d01d35b08447d6f19823835951bffb
Roberto Mota <roberto.motad@gmail.com>
2024-10-24 20:37:45 -0600

    notifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NotificationModule } from './modules/notification/notification.module';
+import { AppLogModule } from './modules/app-log/app-log.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadChildren: ()=> NotificationModule},
+    {path: 'app-log', loadChildren: ()=> AppLogModule}
+];
